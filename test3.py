f48abb32280656c091b50d0d6d41394d2a9ab19c
andriy <andriytsap@gmail.com>
2025-05-17 18:56:14 +0200

    додано ще одне тестування

---
+++ test3.py
@@ -0,0 +1,15 @@
+import requests
+
+def regression_test(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"Тест пройшов успішно: {url} відповідає зі статусом 200.")
+        else:
+            print(f"Помилка: сторінка повернула статус {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Виникла помилка при запиті: {e}")
+
+if __name__ == "__main__":
+    url = "http://127.0.0.1:5500/index.html"  
+    regression_test(url)
